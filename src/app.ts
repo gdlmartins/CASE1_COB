9f2ed1455a1f70da50df160974156ca3a0e94259
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-05-21 02:01:15 +0800

    deploy

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { Express } from "express";
 import cors from "cors";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 
 export const app: Express = express();
@@ -8,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.listen("3003", () => {
+app.listen(process.env.PORT , () => {
     console.log("Server is running on port 3003!")
 })
 
