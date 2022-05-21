75eec175811c73a1646489a9de3a785275f6ce56
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-05-21 14:23:48 +0800

    Master (#1)
    * database try catch
    
    * preparing for deployment
    
    * deploy
    
    * testing deploy heroku
    
    * testing deploy heroku
    
    * testing deploy heroku
    
    * testing deploy heroku
    
    * testing deploy heroku

--- src/database/CompetitorDatabase.ts
+++ src/database/CompetitorDatabase.ts
@@ -9,61 +9,72 @@ let competitors: Competitors[] = []
 
 export class CompetitorDatabase extends BaseDatabase {
     public inserting = async (input: Competitors): Promise<void> => {
-
-        const table_name = "case_1_competitors"
-
-        const competitor: CompetitorOutputDTO = {
-            id: input.id,
-            competition: input.competition,
-            name: input.name,
-            value: input.value,
-            times: input.times
-        }
-        await BaseDatabase.connection(table_name)
+        try {
+         
+            const table_name = "case_1_competitors"
+            
+            const competitor: CompetitorOutputDTO = {
+                id: input.id,
+                competition: input.competition,
+                name: input.name,
+                value: input.value,
+                times: input.times
+            }
+            await BaseDatabase.connection(table_name)
             .insert(competitor)
-    }
-    public insertingNewValue = async (input: Competitors): Promise<void> => {
-
-        const table_name = "case_1_competitors"
-
-        const competitor: CompetitorOutputDTO = {
-            id: input.id,
-            competition: input.competition,
-            name: input.name,
-            value: input.value,
-            times: input.times
+        } catch(error: any){ 
+           throw new Error(error.sqlmessage || error.message);
+       }
         }
-        await BaseDatabase.connection(table_name)
+        public insertingNewValue = async (input: Competitors): Promise<void> => {
+            try {
+                
+                const table_name = "case_1_competitors"
+                
+                const competitor: CompetitorOutputDTO = {
+                    id: input.id,
+                    competition: input.competition,
+                    name: input.name,
+                    value: input.value,
+                    times: input.times
+            }
+            await BaseDatabase.connection(table_name)
             .where({ name: competitor.name })
             .update({ value: competitor.value })
             .then(
                 await BaseDatabase.connection(table_name)
-                    .where({ name: competitor.name })
-                    .update({ times: competitor.times })
-            )
-    }
-    public gettingCompetitorByName = async (input: string): Promise<CompetitorwId | undefined> => {
-
-        const table_name = "case_1_competitors"
-        const name = input;
-
-        const [competitorFromDb]: CompetitorFRomDBDTO[] =
-            await BaseDatabase.connection(table_name)
-                .where({ name })
-
-        if (competitorFromDb) {
-            const competitor: CompetitorwId = {
-                id: competitorFromDb.id,
-                competition: competitorFromDb.competition,
-                name: competitorFromDb.name,
-                value: competitorFromDb.value,
-                times: competitorFromDb.times
+                .where({ name: competitor.name })
+                .update({ times: competitor.times })
+                )
+            } catch(error: any){ 
+                throw new Error(error.sqlmessage || error.message);
             }
-            return competitor;
+        }
+            public gettingCompetitorByName = async (input: string): Promise<CompetitorwId | undefined> => {
+                try{
+                    const table_name = "case_1_competitors"
+                    const name = input;
+                    
+                    const [competitorFromDb]: CompetitorFRomDBDTO[] =
+                    await BaseDatabase.connection(table_name)
+                    .where({ name })
+                    
+                    if (competitorFromDb) {
+                        const competitor: CompetitorwId = {
+                            id: competitorFromDb.id,
+                            competition: competitorFromDb.competition,
+                            name: competitorFromDb.name,
+                            value: competitorFromDb.value,
+                            times: competitorFromDb.times
+                        }
+                        return competitor;
+        }
+    }catch(error: any){ 
+            throw new Error(error.sqlmessage || error.message);
         }
     }
     public gettingRanking = async (input: RackingInputDTO): Promise<any> => {
-
+      try{
         const table_name = "case_1_competitors"
 
         const competition: RackingOutDTO = {
@@ -72,6 +83,9 @@ export class CompetitorDatabase extends BaseDatabase {
         const result = await BaseDatabase.connection(table_name)
             .where(competition).orderBy("value")
         return result
+    } catch(error: any){ 
+        throw new Error(error.sqlmessage || error.message);
     }
+    } 
 
 }
